0c92775cbbe93f1a86fe0792d55334dd23e6e26c
vtrubets <trubetskoy.vasa@gmail.com>
2014-01-13 13:58:16 -0600

    added command line options for generating plots

--- python/qc.pipeline.py
+++ python/qc.pipeline.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env
 
-
 import subprocess as sp
 import gzip as gz
 import optparse as opt
@@ -22,7 +21,6 @@ def smart_vcftools(call):
     '''
     for idx, element in enumerate(call):
         if element.endswith('.vcf.gz'):
-            print element
             call[idx-1] = '--gzvcf'
     return call
 
@@ -53,20 +51,80 @@ def main():
     parser = opt.OptionParser()
     parser.add_option('--vcf', dest = 'vcfFile', action = 'store', 
         help = 'File path for vcf for which to generate QC metrics.')
-    parser.add_option('--intermed-base', dest = 'intermedBase', action = 'store', 
-        help = 'File path base for storage of intermediate files.')
+    parser.add_option('--ped-file', dest = 'pedFile', action = 'store', 
+        help = 'Pedigree file for samples (Optional).')
+    parser.add_option('--tstv-out', dest = 'tstvOut', action = 'store', 
+        help = 'Output file location for TsTv plots PDF.')
+    parser.add_option('--het-out', dest = 'hetOut', action = 'store', 
+        help = 'Output file location for heterozygosity plots PDF.')
+    parser.add_option('--maf-out', dest = 'mafOut', action = 'store', 
+        help = 'Output file location for minor allele frequency plots PDF.')
+    parser.add_option('--miss-out', dest = 'missOut', action = 'store', 
+        help = 'Output file location for missingess plots PDF.')
+    parser.add_option('--rediscover-out', dest = 'kgOut', action = 'store',
+        help = 'Output file location for rediscovery rate plots PDF.')
+    parser.add_option('--hardy-out', dest = 'hardyOut', action = 'store',
+        help = 'Output file location for Hardy Weinberg analysis plots PDF.')
+    parser.add_option('--mendel-out', dest = 'mendelOut', action = 'store', 
+        help = 'Output file location for Mendel inconsistency plots PDF.')
+    parser.add_option('--temp-dir', dest = 'tempDir', action = 'store',
+        help = 'Directory for writing intermediate analysis files.')
     (options, args) = parser.parse_args()
 
     vcfFile = options.vcfFile
-    intermedBase = options.intermedBase
+    intermedBase = options.tempDir + 'qc'
+
+
+    ## convert vcf to plink formatted files
+    convertCall = ['vcftools', '--vcf', vcfFile, '--plink', '--recode', '--out',
+        intermedBase]
+    convertCall = smart_vcftools(convertCall)
+    sp.call(convertCall)
+
+    if options.pedFile:   
+      ## pull pedigree information from previous run
+      pedFile = options.vcfFile 
+      mothers = {}
+      fathers = {}
+      families = {}
+      sexes = {}
+      with open(pedFile) as fconn:
+        for line in fconn:
+          line = line.split(',')
+          mother = line[3]
+          father = line[2]
+          if mother=='Not Sequenced':
+              mother = '0'
+          if father=='Not Sequenced':
+              father = '0'
+          fathers[line[1]] = father
+          mothers[line[1]] = mother
+          family = line[0] 
+          families[line[1]] = family
+          sexes[line[1]] = line[4]
+
+      ## add pedigree information to plink ped file
+      plinkFile = intermedBase + '.pedigree.ped'
+      plinkConn = open(plinkFile, 'w')
+      with smart_open(intermedBase + '.ped') as plinkFile:
+          for line in plinkFile:
+              line = line.strip().split('\t')
+              iid = line[1]
+              if fathers.get(iid):
+                  line[2] = fathers[iid]
+              if mothers.get(iid):
+                  line[3] = mothers[iid]
+              if families.get(iid):
+                  line[0] = families[iid]
+              if sexes.get(iid):
+                  line[4] = sexes[iid]
+              ## add a fake phenotype
+              line[5] = '2'
+              print >> plinkConn, '\t'.join(line)
+      plinkConn.close()
 
     ## identify the total number of variants in the VCF file
-    zgrepCall = ['zgrep', '-v', '\'#\'', vcfFile]
-    wcCall = ['wc', '-l']
-    zgrep = sp.Popen( zgrepCall, stdout = sp.PIPE )
-    wc = sp.Popen( wcCall, stdin = zgrep.stdout, stdout = sp.PIPE )
-    zgrep.stdout.close()
-    nsnp = wc.communicate()[0]
+    nsnp = len( [ rec for rec in vcf.Reader(open(vcfFile, 'r')) ] )
 
     ## get the ts/tv ratio
     tstvCall = ['vcftools', '--vcf', vcfFile, '--TsTv', nsnp, '--out',
@@ -76,59 +134,10 @@ def main():
     ## Calculate sample heterozygosity
     hetCall = ['vcftools', '--vcf', vcfFile, '--het', '--out', intermedBase ]
     hetCall = smart_vcftools(hetCall)
-
-    ## convert vcf to plink formatted files
-    convertCall = ['vcftools', '--vcf', vcfFile, '--plink', '--recode', '--out',
-        intermedBase]
-    convertCall = smart_vcftools(convertCall)
-    sp.call(convertCall)
-   
-    ## pull pedigree information from previous run
-    pedFile = '/nas40t0/vasya/autism/data/ha_vcf_trios_pedfile_012813_nick_vcf_trios.csv'
-    mothers = {}
-    fathers = {}
-    families = {}
-    sexes = {}
-    with open(pedFile) as fconn:
-      for line in fconn:
-        line = line.split(',')
-            
-        mother = line[3]
-        father = line[2]
-        if mother=='Not Sequenced':
-            mother = '0'
-        if father=='Not Sequenced':
-            father = '0'
-
-        fathers[line[1]] = father
-        mothers[line[1]] = mother
-        family = line[0] 
-        families[line[1]] = family
-        sexes[line[1]] = line[4]
-
-    ## add pedigree information to plink ped file
-    newPlinkFile = intermedBase + '.pedigree.ped'
-    newPlinkConn = open(newPlinkFile, 'w')
-    with smart_open(intermedBase + '.ped') as plinkFile:
-        for line in plinkFile:
-            line = line.strip().split('\t')
-            iid = line[1]
-            if fathers.get(iid):
-                line[2] = fathers[iid]
-            if mothers.get(iid):
-                line[3] = mothers[iid]
-            if families.get(iid):
-                line[0] = families[iid]
-            if sexes.get(iid):
-                line[4] = sexes[iid]
-            ## add a fake phenotype
-            line[5] = '2'
-            print >> newPlinkConn, '\t'.join(line)
-    newPlinkConn.close()
  
     ## calculate mendelian errors
     mendelCall = ['plink',
-        '--ped', newPlinkFile,
+        '--ped', plinkFile,
         '--map', intermedBase + '.map',
         '--mendel',
         '--allow-no-sex',
@@ -159,26 +168,34 @@ def main():
     '--out', intermedBase]
     
 
-    ## calculate EVS rediscovery rate
-    evsVar = cPickle.load(open('/nas40t0/vasya/exome_variant_server/ESP6500SI-V2-SSA137.snps.set.pickle', 'rb'))
-    evsRes = calc_rediscovery(evsVar, vcfFile)
-    del evsVar
-    print >> open(intermedBase+'.evs', 'w'), '\t'.join(evsRes)
+    ## call external binaries and generate plots
+    if options.rediscoverOut:
+      ## calculate EVS rediscovery rate
+      evsVar = cPickle.load(open('/nas40t0/vasya/exome_variant_server/ESP6500SI-V2-SSA137.snps.set.pickle', 'rb'))
+      evsRes = calc_rediscovery(evsVar, vcfFile)
+      del evsVar
+      print >> open(intermedBase+'.evs', 'w'), '\t'.join(evsRes)
+
+      ## calculate 1kG rediscovery rate
+      kgVar = cPickle.load(open('/nas40t0/vasya/1kG/ALL_1000G_phase1integrated_v3_impute_var.pickle'))
+      kgRes = calc_rediscovery(kgVar, vcfFile) 
+      del kgVar
+      print >> open(intermedBase+'.kg', 'w'), '\t'.join(kgRes)
 
 
-    ## calculate 1kG rediscovery rate
-    kgVar = cPickle.load(open('/nas40t0/vasya/1kG/ALL_1000G_phase1integrated_v3_impute_var.pickle'))
-    kgRes = calc_rediscovery(kgVar, vcfFile) 
-    del kgVar
-    print >> open(intermedBase+'.kg', 'w'), '\t'.join(kgRes)
-
-    ## call plink and vcftools based statistics
-    sp.call(mendelCall)
-    sp.call(tstvCall)
-    sp.call(hetCall)
-    sp.call(hweCall)
-    sp.call(missCall)
-    sp.call(mafCall)
+    ## call external binaries and generate plots 
+    if options.mendelOut:
+      sp.call(mendelCall)
+    if options.tstvOut:
+      sp.call(tstvCall)
+    if options.hetOut:
+      sp.call(hetCall)
+    if options.hardyOut:
+      sp.call(hweCall)
+    if options.missOut:
+      sp.call(missCall)
+    if options.mafOut:
+      sp.call(mafCall)
 
 
 if __name__ == '__main__':
