73bb4bf81de5d74aa86a76b585a40f962ff30a92
vtrubets <trubetskoy.vasa@gmail.com>
2014-01-13 10:12:10 -0600

    Updated python script to match the code used to generate the publication plots

--- python/qc.pipeline.py
+++ python/qc.pipeline.py
@@ -1,13 +1,11 @@
-'''
-This is going to be messy.
+#!/usr/bin/env
 
-String together vcftools, plink, and python to produce a set of QC metrics on a
-vcf.
-'''
 
 import subprocess as sp
 import gzip as gz
 import optparse as opt
+import cPickle
+import vcf
 
 def smart_open(f):
     '''
@@ -28,6 +26,27 @@ def smart_vcftools(call):
             call[idx-1] = '--gzvcf'
     return call
 
+def makeID(rec, altall):
+  var = rec.CHROM.lstrip('chr') + ':' + str(rec.POS) + '.' + rec.REF + '.' + str(altall)
+  altvar = rec.CHROM.lstrip('chr') + ':' + str(rec.POS) + '.' + str(altall) + '.' + rec.REF
+  return var, altvar
+
+def calc_rediscovery(evs, vcfFile):
+  '''
+  Calculate the variant rediscovery rate in a VCF file.
+  '''
+  count = 0
+  total = 0
+  vcfReader = vcf.Reader(open(vcfFile))
+  for rec in vcfReader:
+    for allele in rec.ALT:
+      var, altvar = makeID(rec, allele)
+      if (var in evs) or (altvar in evs):
+        count += 1
+      total += 1
+  return (vcfFile.split('/')[-1], str(float(count)/float(total)))
+
+
 def main():
 
     ## parse command line arguments
@@ -41,12 +60,6 @@ def main():
     vcfFile = options.vcfFile
     intermedBase = options.intermedBase
 
-    devnull = open('/dev/null', 'w')
-    sampleKeyFile = '/nas40t2/tsExoms/Sample Info/Scharf_master_sequencing_sample_key.csv'
-    pedFile = '/nas40t2/tsExoms/Sample Info/updated_Scharf_Pedigree.csv'
-
-
-
     ## identify the total number of variants in the VCF file
     zgrepCall = ['zgrep', '-v', '\'#\'', vcfFile]
     wcCall = ['wc', '-l']
@@ -55,88 +68,51 @@ def main():
     zgrep.stdout.close()
     nsnp = wc.communicate()[0]
 
-
     ## get the ts/tv ratio
     tstvCall = ['vcftools', '--vcf', vcfFile, '--TsTv', nsnp, '--out',
         intermedBase + '.tstv']
     tstvCall = smart_vcftools(tstvCall)
-    #print ' '.join(tstvCall)
-    #sp.call(tstvCall)
 
-
+    ## Calculate sample heterozygosity
+    hetCall = ['vcftools', '--vcf', vcfFile, '--het', '--out', intermedBase ]
+    hetCall = smart_vcftools(hetCall)
 
     ## convert vcf to plink formatted files
     convertCall = ['vcftools', '--vcf', vcfFile, '--plink', '--recode', '--out',
         intermedBase]
     convertCall = smart_vcftools(convertCall)
-    #sp.call(convertCall)
-    
-    ## extract qual scores from INFO field
-    qualCall = ['vcftools', '--vcf', vcfFile,
-    '--get-INFO', 'AQ', '--get-INFO', 'GQ', '--get-INFO', 'FQ',
-    '--out', intermedBase]
-    sp.call(smart_vcftools(qualCall))
-
-    ## store mapping of SM to RG
-    rg_sm = dict()
-    sm_rg = dict()
-    fconn = smart_open(sampleKeyFile)
-    header = fconn.next()
-    for line in fconn:
-       line = line.split(',')
-       rg_sm[line[1]] = line[0]
-       sm_rg[line[0]] = line[1]  
-    fconn.close()
-
-    ## add pedigree information to plink ped file file
-    mothers = dict()
-    fathers = dict()
-    families = dict()
-    sexes = dict()
-    with smart_open(pedFile) as fconn:
-        header = enumerate(fconn.next().split(','))
-        index = dict( ( (x[1], x[0]) for x in header ) )
-        for line in fconn:
-            line = line.split(',')
-            if rg_sm.get(line[index['Mother']]):
-                mother = rg_sm[ line[index['Mother']] ]
-            else:
-                mother = line[index['Mother']]
-
-            if rg_sm.get(line[index['Father']]):
-                father = rg_sm[ line[index['Father']]]
-            else:
-                father = line[index['Father']]
+    sp.call(convertCall)
+   
+    ## pull pedigree information from previous run
+    pedFile = '/nas40t0/vasya/autism/data/ha_vcf_trios_pedfile_012813_nick_vcf_trios.csv'
+    mothers = {}
+    fathers = {}
+    families = {}
+    sexes = {}
+    with open(pedFile) as fconn:
+      for line in fconn:
+        line = line.split(',')
             
-            if mother=='Not Sequenced':
-                mother = '0'
-            if father=='Not Sequenced':
-                father = '0'
+        mother = line[3]
+        father = line[2]
+        if mother=='Not Sequenced':
+            mother = '0'
+        if father=='Not Sequenced':
+            father = '0'
 
-            fathers[line[index['SM_tag (Local_ID)']]] = father
-            mothers[line[index['SM_tag (Local_ID)']]] = mother
+        fathers[line[1]] = father
+        mothers[line[1]] = mother
+        family = line[0] 
+        families[line[1]] = family
+        sexes[line[1]] = line[4]
 
-            if rg_sm.get(line[index['Individ']]):
-                family = line[index['Family']]
-            else:
-                family = line[index['SM_tag (Local_ID)']] 
-
-            families[line[index['SM_tag (Local_ID)']]] = family
-
-            sexes[line[index['SM_tag (Local_ID)']]] = line[index['Sex']]
-
-
+    ## add pedigree information to plink ped file
     newPlinkFile = intermedBase + '.pedigree.ped'
     newPlinkConn = open(newPlinkFile, 'w')
     with smart_open(intermedBase + '.ped') as plinkFile:
         for line in plinkFile:
             line = line.strip().split('\t')
-            ## convert ID from RG to SM if necessary
-            if rg_sm.get(line[1]):
-              iid = rg_sm[line[1]]
-              line[1] = iid
-            else:
-              iid = line[1]
+            iid = line[1]
             if fathers.get(iid):
                 line[2] = fathers[iid]
             if mothers.get(iid):
@@ -144,12 +120,12 @@ def main():
             if families.get(iid):
                 line[0] = families[iid]
             if sexes.get(iid):
-                line[4] = sexes[iid] 
+                line[4] = sexes[iid]
             ## add a fake phenotype
             line[5] = '2'
             print >> newPlinkConn, '\t'.join(line)
     newPlinkConn.close()
-
+ 
     ## calculate mendelian errors
     mendelCall = ['plink',
         '--ped', newPlinkFile,
@@ -157,38 +133,52 @@ def main():
         '--mendel',
         '--allow-no-sex',
         '--out', intermedBase]
-    #sp.call(mendelCall)# stdout = devnull, stderr = devnull)
 
     ## generate minor allele frequency distribution
     mafCall = ['plink',
-    '--ped', newPlinkFile,
+    '--ped', intermedBase + '.pedigree.ped',
     '--map', intermedBase + '.map',
     '--freq',
     '--allow-no-sex',
     '--out', intermedBase]
-    #sp.call(mafCall)
 
     
     ## get the missingness rates
     missCall = ['plink',
-    '--ped', newPlinkFile,
+    '--ped', intermedBase + '.pedigree.ped',
     '--map', intermedBase + '.map',
     '--missing',
     '--out', intermedBase]
-    #sp.call(missCall)
 
 
     ## get hardy weinberg estimates
     hweCall = ['plink',
-    '--ped', newPlinkFile,
+    '--ped', intermedBase + '.pedigree.ped',
     '--map', intermedBase + '.map',
     '--hardy',
     '--out', intermedBase]
-    #sp.call(hweCall)
+    
+
+    ## calculate EVS rediscovery rate
+    evsVar = cPickle.load(open('/nas40t0/vasya/exome_variant_server/ESP6500SI-V2-SSA137.snps.set.pickle', 'rb'))
+    evsRes = calc_rediscovery(evsVar, vcfFile)
+    del evsVar
+    print >> open(intermedBase+'.evs', 'w'), '\t'.join(evsRes)
 
 
-    devnull.close()
+    ## calculate 1kG rediscovery rate
+    kgVar = cPickle.load(open('/nas40t0/vasya/1kG/ALL_1000G_phase1integrated_v3_impute_var.pickle'))
+    kgRes = calc_rediscovery(kgVar, vcfFile) 
+    del kgVar
+    print >> open(intermedBase+'.kg', 'w'), '\t'.join(kgRes)
 
+    ## call plink and vcftools based statistics
+    sp.call(mendelCall)
+    sp.call(tstvCall)
+    sp.call(hetCall)
+    sp.call(hweCall)
+    sp.call(missCall)
+    sp.call(mafCall)
 
 
 if __name__ == '__main__':
